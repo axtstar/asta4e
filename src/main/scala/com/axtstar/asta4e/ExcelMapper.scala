3a4cf4f33738b7d996b54b4e83bcf843dc2f9a44
axt★ <axtstart@gmail.com>
2018-07-13 00:09:21 +0900

    separated to trait

--- src/main/scala/com/axtstar/asta4e/ExcelMapper.scala
+++ src/main/scala/com/axtstar/asta4e/ExcelMapper.scala
@@ -1,10 +1,10 @@
 package com.axtstar.asta4e
 
-import com.axtstar.asta4e.core.ExcelBasic
+import com.axtstar.asta4e.core.{ExcelBasic, Helper}
 import shapeless._
 import labelled.{FieldType, field}
 
-object ExcelMapper extends ExcelBasic {
+object ExcelMapper extends ExcelBasic with Helper {
 
   /**
     * case class to Map
@@ -31,49 +31,6 @@ object ExcelMapper extends ExcelBasic {
         case _ =>
           throw new IllegalArgumentException
       }
-    }
-  }
-
-  trait FromMap[L <: HList] {
-    def apply(m: Map[String, Any]): Option[L]
-  }
-
-  trait LowPriorityFromMap {
-    implicit def hconsFromMap1[K <: Symbol, V, T <: HList](implicit
-                                                           witness: Witness.Aux[K],
-                                                           typeable: Typeable[V],
-                                                           fromMapT: Lazy[FromMap[T]]
-                                                          ): FromMap[FieldType[K, V] :: T] = new FromMap[FieldType[K, V] :: T] {
-
-      def apply(m: Map[String, Any]): Option[FieldType[K, V] :: T] = {
-        val result = for {
-          v <- m.get(witness.value.name)
-          h <- typeable.cast(v)
-          t <- fromMapT.value(m)
-        } yield field[K](h) :: t
-        result
-
-      }
-    }
-  }
-
-  object FromMap extends LowPriorityFromMap {
-    implicit val hnilFromMap: FromMap[HNil] = new FromMap[HNil] {
-      def apply(m: Map[String, Any]): Option[HNil] = Some(HNil)
-    }
-
-    implicit def hconsFromMap0[K <: Symbol, V, R <: HList, T <: HList](implicit
-                                                                       witness: Witness.Aux[K],
-                                                                       gen: LabelledGeneric.Aux[V, R],
-                                                                       fromMapH: FromMap[R],
-                                                                       fromMapT: FromMap[T]
-                                                                      ): FromMap[FieldType[K, V] :: T] = new FromMap[FieldType[K, V] :: T] {
-      def apply(m: Map[String, Any]): Option[FieldType[K, V] :: T] = for {
-        v <- m.get(witness.value.name)
-        r <- Typeable[Map[String, Any]].cast(v)
-        h <- fromMapH(r)
-        t <- fromMapT(m)
-      } yield field[K](gen.from(h)) :: t
     }
   }
 
--- src/main/scala/com/axtstar/asta4e/core/Helper.scala
+++ src/main/scala/com/axtstar/asta4e/core/Helper.scala
@@ -1,6 +1,50 @@
 package com.axtstar.asta4e.core
 
+import shapeless.{::, HList, HNil, LabelledGeneric, Lazy, Typeable, Witness}
+import shapeless.labelled.{FieldType, field}
+
 trait Helper {
 
+  trait FromMap[L <: HList] {
+    def apply(m: Map[String, Any]): Option[L]
+  }
 
+  trait LowPriorityFromMap {
+    implicit def hconsFromMap1[K <: Symbol, V, T <: HList](implicit
+                                                           witness: Witness.Aux[K],
+                                                           typeable: Typeable[V],
+                                                           fromMapT: Lazy[FromMap[T]]
+                                                          ): FromMap[FieldType[K, V] :: T] = new FromMap[FieldType[K, V] :: T] {
+
+      def apply(m: Map[String, Any]): Option[FieldType[K, V] :: T] = {
+        val result = for {
+          v <- m.get(witness.value.name)
+          h <- typeable.cast(v)
+          t <- fromMapT.value(m)
+        } yield field[K](h) :: t
+        result
+
+      }
+    }
+  }
+
+  object FromMap extends LowPriorityFromMap {
+    implicit val hnilFromMap: FromMap[HNil] = new FromMap[HNil] {
+      def apply(m: Map[String, Any]): Option[HNil] = Some(HNil)
+    }
+
+    implicit def hconsFromMap0[K <: Symbol, V, R <: HList, T <: HList](implicit
+                                                                       witness: Witness.Aux[K],
+                                                                       gen: LabelledGeneric.Aux[V, R],
+                                                                       fromMapH: FromMap[R],
+                                                                       fromMapT: FromMap[T]
+                                                                      ): FromMap[FieldType[K, V] :: T] = new FromMap[FieldType[K, V] :: T] {
+      def apply(m: Map[String, Any]): Option[FieldType[K, V] :: T] = for {
+        v <- m.get(witness.value.name)
+        r <- Typeable[Map[String, Any]].cast(v)
+        h <- fromMapH(r)
+        t <- fromMapT(m)
+      } yield field[K](gen.from(h)) :: t
+    }
+  }
 }
