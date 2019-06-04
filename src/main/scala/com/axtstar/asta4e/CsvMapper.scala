ea04ad7fd93c4659286ac36bd4c63af7d5fd8690
axt <axt_star@hotmail.com>
2019-06-04 10:04:24 +0900

    add function on CsvMapper

--- src/main/scala/com/axtstar/asta4e/CsvMapper.scala
+++ src/main/scala/com/axtstar/asta4e/CsvMapper.scala
@@ -30,7 +30,7 @@ object CsvMapper extends CsvBasic {
   *
   * @tparam A1
   */
-class CsvMapper[A1] extends CsvBasic with TypeCore[A1] {
+class CsvMapper[A1] extends CsvBasic with TypeCore[A1] with FTypeCore[A1] {
   def withLocation(_locationMap: List[Location]) = {
     super.withLocation(_locationMap)
     this
@@ -190,5 +190,176 @@ class CsvMapper[A1] extends CsvBasic with TypeCore[A1] {
 
   }
 
+  override def getFCC[RA1 <: HList, KA1 <: HList, VA1 <: HList, MA1 <: HList,
+  A2, RA2 <: HList, KA2 <: HList, VA2 <: HList, MA2 <: HList]
+  (iStream: FileInputStream)
+  (f: Option[A2] => Option[A1])
+  (implicit
+   gen: LabelledGeneric.Aux[A1, RA1],
+   fromMap: FromMap[RA1],
+   typeable: Typeable[A1],
+   keys: Keys.Aux[RA1, KA1],
+   ktl: hlist.ToList[KA1, Symbol],
+   values: Values.Aux[RA1, VA1],
+   mapper: hlist.Mapper.Aux[typeablePoly.type, VA1, MA1],
+   fillWith: hlist.FillWith[nullPoly.type, VA1],
+   vtl: hlist.ToList[MA1, String],
+
+   gen2: LabelledGeneric.Aux[A2, RA2],
+   fromMap2: FromMap[RA2],
+   typeable2: Typeable[A2],
+   keys2: Keys.Aux[RA2, KA2],
+   ktl2: hlist.ToList[KA2, Symbol],
+   values2: Values.Aux[RA2, VA2],
+   mapper2: hlist.Mapper.Aux[typeablePoly.type, VA2, MA2],
+   fillWith2: hlist.FillWith[nullPoly.type, VA2],
+   vtl2: hlist.ToList[MA2, String]
+
+  ): IndexedSeq[(String, Option[A1])]={
+    val columns = ktl2(keys2())
+
+    super._getData(iStream).map {
+      x =>
+
+        //Type cast
+        val m = x._2
+
+        val target = fromMap2(columns.map { x =>
+          x.name -> (
+            if (m.contains(x.name)) {
+              m(x.name)
+            } else {
+              null
+            })
+        }.toMap).map {
+          x =>
+            gen2.from(x)
+        }
+
+        x._1 -> f(Option(
+          if (typeable2.describe.startsWith("Option")) {
+            target.get
+          } else {
+            target match {
+              case Some(tt) =>
+                tt
+              case _ =>
+                None.asInstanceOf[A2]
+            }
+          }
+        ))
+      //
+    }
+
+  }
+
+  override def getFCCDown[RA1 <: HList, KA1 <: HList, VA1 <: HList, MA1 <: HList,
+  A2, RA2 <: HList, KA2 <: HList, VA2 <: HList, MA2 <: HList]
+  (iStream: FileInputStream)
+  (f: Option[A2] => Option[A1])
+  (implicit
+   gen: LabelledGeneric.Aux[A1, RA1],
+   fromMap: FromMap[RA1],
+   typeable: Typeable[A1],
+   keys: Keys.Aux[RA1, KA1],
+   ktl: hlist.ToList[KA1, Symbol],
+   values: Values.Aux[RA1, VA1],
+   mapper: hlist.Mapper.Aux[typeablePoly.type, VA1, MA1],
+   fillWith: hlist.FillWith[nullPoly.type, VA1],
+   vtl: hlist.ToList[MA1, String],
+
+   gen2: LabelledGeneric.Aux[A2, RA2],
+   fromMap2: FromMap[RA2],
+   typeable2: Typeable[A2],
+   keys2: Keys.Aux[RA2, KA2],
+   ktl2: hlist.ToList[KA2, Symbol],
+   values2: Values.Aux[RA2, VA2],
+   mapper2: hlist.Mapper.Aux[typeablePoly.type, VA2, MA2],
+   fillWith2: hlist.FillWith[nullPoly.type, VA2],
+   vtl2: hlist.ToList[MA2, String]
+
+  ): IndexedSeq[(String, IndexedSeq[Option[A1]])] = {
+    val columns = ktl2(keys2())
+
+    super._getDataDown(iStream).map {
+      x =>
+        x._1 ->
+          x._2.map {
+            xx =>
+              //Type cast
+              val m = xx
+
+              val target = fromMap2(columns.map { xxx =>
+                xxx.name -> (
+                  if (m.contains(xxx.name)) {
+                    m(xxx.name)
+                  } else {
+                    null
+                  })
+              }.toMap).map {
+                x =>
+                  gen2.from(x)
+              }
+
+              f(Option(
+                if (typeable2.describe.startsWith("Option")) {
+                  target.get
+                } else {
+                  target match {
+                    case Some(tt) =>
+                      tt
+                    case _ =>
+                      None.asInstanceOf[A2]
+                  }
+                }
+              ))
+            //
+
+          }
+    }
+
+  }
+
+  override def setFCC[RA1 <: HList, A2 , RA2 <: HList]
+  (bindCC: IndexedSeq[(String, Option[A2])])
+  (f: Option[A2] => Option[A1])
+  (implicit
+   gen: LabelledGeneric.Aux[A1, RA1],
+   tmr: ToMap[RA1],
+
+   gen2: LabelledGeneric.Aux[A2, RA2],
+   tmr2: ToMap[RA2]
+
+  ): Unit = {
+    val map = bindCC.map {
+      x =>
+        x._1 -> CC.By(f(x._2).get).toMap
+    }
+    super._setData(map: _*)
+
+  }
+
+  override def setFCCDown[RA1 <: HList, A2, RA2 <: HList]
+  (bindCC: IndexedSeq[(String, IndexedSeq[Option[A2]])])
+  (f: Option[A2] => Option[A1])
+  (implicit
+   gen: LabelledGeneric.Aux[A1, RA1],
+   tmr: ToMap[RA1],
+
+   gen2: LabelledGeneric.Aux[A2, RA2],
+   tmr2: ToMap[RA2]
+
+  ): Unit = {
+    val map = bindCC.map {
+      x =>
+        x._1 -> (x._2.map {
+          xx =>
+            CC.By(f(xx).get).toMap
+        })
+    }
+
+    super._setDataDown(map: _*)
+
+  }
 
 }
